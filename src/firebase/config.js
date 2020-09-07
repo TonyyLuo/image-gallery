d7bcf97c89e753854deb4ae85faee0a79c197620
TonyLuo <tonyluolang@gmail.com>
2020-09-07 16:05:27 -0400

    Add images to firestore

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
-import * as firebase from 'firebase/app';
-import 'firebase/storage';
-import 'firebase/firestore';
+import * as firebase from 'firebase/app'
+import 'firebase/storage'
+import 'firebase/firestore'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
@@ -11,11 +11,12 @@ var firebaseConfig = {
     storageBucket: "image-gallery-ff833.appspot.com",
     messagingSenderId: "642462165623",
     appId: "1:642462165623:web:40cad4dd561b50a4caf472"
-};
+}
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
 
-const projectStorage = firebase.storage();
-const projectFirestore = firebase.firestore();
+const projectStorage = firebase.storage()
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
 
-export { projectStorage, projectFirestore };
+export { projectStorage, projectFirestore, timestamp }
