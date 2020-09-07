4c643f09706c7bd59aae87389f5d53a3540669c6
TonyLuo <tonyluolang@gmail.com>
2020-09-07 14:56:05 -0400

    Added Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC9Jt7AZhcdPC3Oqd9YgjrcJOkZrz74QqM",
+    authDomain: "image-gallery-ff833.firebaseapp.com",
+    databaseURL: "https://image-gallery-ff833.firebaseio.com",
+    projectId: "image-gallery-ff833",
+    storageBucket: "image-gallery-ff833.appspot.com",
+    messagingSenderId: "642462165623",
+    appId: "1:642462165623:web:40cad4dd561b50a4caf472"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
