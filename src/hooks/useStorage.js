1fc7793b4ce70735a8ff6c920738113083d1cd30
TonyLuo <tonyluolang@gmail.com>
2020-09-08 14:19:37 -0400

    Deletion works

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,30 +1,39 @@
-import { useState, useEffect } from 'react'
-import { projectStorage, projectFirestore, timestamp } from '../firebase/config'
+import { useState, useEffect } from "react";
+import {
+  projectStorage,
+  projectFirestore,
+  timestamp,
+} from "../firebase/config";
 
 const useStorage = (file) => {
-    const [progress, setProgress] = useState(0);
-    const [error, setError] = useState(null)
-    const [url, setUrl] = useState(null)
+  const [progress, setProgress] = useState(0);
+  const [error, setError] = useState(null);
+  const [url, setUrl] = useState(null);
 
-    useEffect(() => {
-        //references
-        const storageRef = projectStorage.ref(file.name)
-        const collectionRef = projectFirestore.collection('gallery1')
+  useEffect(() => {
+    //references
+    const storageRef = projectStorage.ref(file.name);
+    const collectionRef = projectFirestore.collection("gallery1");
 
-        storageRef.put(file).on('state_changed', (snap) => {
-            let percentage = (snap.bytesTransferred / snap.totalBytes) * 100
-            setProgress(percentage);
-        }, (err) => {
-            setError(err)
-        }, async () => {
-            const url = await storageRef.getDownloadURL()
-            const createdAt = timestamp();
-            collectionRef.add({ url, createdAt })
-            setUrl(url)
-        })
-    }, [file])
+    storageRef.put(file).on(
+      "state_changed",
+      (snap) => {
+        let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+        setProgress(percentage);
+      },
+      (err) => {
+        setError(err);
+      },
+      async () => {
+        const url = await storageRef.getDownloadURL();
+        const createdAt = timestamp();
+        collectionRef.add({ url, createdAt });
+        setUrl(url);
+      }
+    );
+  }, [file]);
 
-    return { progress, url, error }
-}
+  return { progress, url, error };
+};
 
-export default useStorage
+export default useStorage;
